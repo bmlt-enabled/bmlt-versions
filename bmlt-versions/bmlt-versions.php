0a2dbd2b68af289cad8611c4b9ead179ff33d46d
Patrick Joyce <pjaudiomv@gmail.com>
2022-09-05 13:22:36 -0400

    fix version

--- bmlt-versions/bmlt-versions.php
+++ bmlt-versions/bmlt-versions.php
@@ -5,7 +5,7 @@ Plugin URI: https://github.com/bmlt-enabled/bmlt-versions/
 Description: A simple content generator to display the versions and links of the various BMLT components. Add [bmlt_versions] to a page or a post to generate the list.
 Author: BMLT Authors
 Author URI: https://bmlt.app
-Version: 1.7.1
+Version: 1.6.1
 Install: Drop this directory into the "wp-content/plugins/" directory and activate it.
 */
 /* Disallow direct access to the plugin file */
--- bmlt-versions/readme.txt
+++ bmlt-versions/readme.txt
@@ -2,7 +2,7 @@
 
 Contributors: pjaudiomv, radius314
 Tags: bmlt, meeting list
-Tested up to: 5.3.2
+Tested up to: 5.6.0
 Stable tag: 1.6.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
